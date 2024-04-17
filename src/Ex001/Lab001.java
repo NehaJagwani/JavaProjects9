ecd6e942ae0c57fd6d42ef7ecea8602d79cd369c
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 16:04:15 +1000

    add

--- src/Ex001/Lab001.java
+++ src/Ex001/Lab001.java
@@ -3,6 +3,6 @@ package Ex001;
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("number is 65");
-        System.out.println("checking git push origin main command");
+        System.out.println("checking git push command");
     }
 }
